749edd527bc38975aa31134721ca0674f0fd5472
egamberganovadhambek <egamberganovadhambek1@gmail.com>
2026-04-11 17:43:12 +0500

    meros olindi

---
+++ meros.php
@@ -0,0 +1,78 @@
+<?php
+
+
+
+class fruit 
+{
+    public $name;
+    public $color;
+    public $year;
+     
+function __construct($name,$color,$year){
+    this->name=$name;
+    this->color=$color;
+    this->year=$year
+}
+function phone(){
+    return "telefon nomi"." " this->name . "color" . " " this->color . " " "ishlab chiqarilgan yili" . this->year; 
+}
+}
+class pppp extends fruit(){
+    public function xabar(){
+        echo "bu telefon";
+    }   
+}
+$iphone = new ("iphone", "qora");  
+$iphone->xabar(); 
+$iphone-><?php
+
+
+
+class fruit 
+{
+    public $name;
+    public $color;
+    public $year;
+     
+function __construct($name,$color,$year){
+    this->name=$name;
+    this->color=$color;
+    this->year=$year
+}
+function phone(){
+    return "telefon nomi"." " this->name . "color" . " " this->color . " " "ishlab chiqarilgan yili" . this->year; 
+}
+}
+class pppp extends fruit(){
+    public function xabar(){
+        echo "bu telefon";
+    }   
+}
+$iphone = new ("iphone", "qora");  
+$iphone->(); 
+$iphone->();<?php
+
+
+
+class fruit 
+{
+    public $name;
+    public $color;
+    public $year;
+     
+function __construct($name,$color,$year){
+    this->name=$name;
+    this->color=$color;
+    this->year=$year
+}
+function phone(){
+    return "telefon nomi"." " this->name . "color" . " " this->color . " " "ishlab chiqarilgan yili" . this->year; 
+}
+}
+class pppp extends fruit(){
+    public function xabar(){
+        echo "bu telefon";
+    }   
+}
+$iphone = new Iphone ("iphone", "qora");  
+$iphone->xabar(); 
